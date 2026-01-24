4165fdd496974fb1cc38d05623ad4a16cdd7abaf
Ayo <Ayoroq@gmail.com>
2026-01-24 02:40:37 -0500

    feat: added the function to get the posts upon loading

--- src/context/PostContext.jsx
+++ src/context/PostContext.jsx
@@ -8,8 +8,32 @@ export function PostProvider({ children }) {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
+  useEffect(() => {
+    async function fetchPosts() {
+      setLoading(true);
+      setError(null);
+      try {
+        const response = await fetch(
+          `${import.meta.env.VITE_BACKEND_URL}/posts`,
+        );
+        if (response.ok) {
+          const data = await response.json();
+          setPosts(data.posts);
+        } else {
+          setError("Failed to fetch posts");
+        }
+      } catch (error) {
+        console.error("Error fetching posts:", error);
+        setError("Failed to fetch posts");
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchPosts();
+  }, []);
+
   return (
-    <PostContext.Provider value={{ posts, loading, error, fetchPosts }}>
+    <PostContext.Provider value={{ posts, loading, error }}>
       {children}
     </PostContext.Provider>
   );
