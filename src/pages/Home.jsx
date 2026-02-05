1c0e07d5fd380344ca9c9ed7a9710683d99cc473
Ayo <Ayoroq@gmail.com>
2026-02-05 01:33:32 -0500

    feat: improved the styling on the homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,12 @@
 import styles from './p.module.css'
+import {useEffect } from 'react';
 export default function Home() {
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'instant' });
+  }, []);
   return (
     <main className={styles.Home}>
-      <h1>Welcome to my blog. I'm Ayo and this is where I document my latest explorations and ideas</h1>
+      <h1 className={styles.mainText}>Welcome to my blog. I'm Ayo and this is where I document my latest explorations, ideas and anything i find interesting.</h1>
     </main>
   );
 }
