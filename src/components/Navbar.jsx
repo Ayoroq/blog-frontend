7fd00496220ed76d021bc87f8e32bb148250fa5e
Ayo <Ayoroq@gmail.com>
2026-02-09 21:28:30 -0500

    feat: added some stylings to the navbar and navigating to main homepage on click

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,10 @@
 import styles from './c.module.css'
+import {useNavigate} from 'react-router'
 export default function Navbar() {
+  const navigate = useNavigate()
   return (
     <nav className={styles.navbar}>
-      <p className={styles.logo}></p>
+      <p onClick={() => (navigate('/'))} className={styles.logo}></p>
       <div></div>
     </nav>
   )
